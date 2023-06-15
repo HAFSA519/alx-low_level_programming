0b3bf4e3c67b4abf1b0d15dbe3b3cf884f2deb61
HAFSA519 <hafsa.fsac@gmail.com>
2023-06-15 13:50:38 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-* main - Entry point
+* main - Prints the size of various typesa based on
 *
-* Return: Always 0 (Success)
+* Return: Always 0. (Success)
 */
 int main(void)
 {
