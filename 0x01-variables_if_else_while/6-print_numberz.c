fd720151441146eff21d28c06220d0db9567f871
HAFSA519 <hafsa.fsac@gmail.com>
2023-06-16 14:56:00 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry the point of the program
+ *
+ * Return: 0 (SUCCESS°
+ */
+
+int main(void)
+{
+	char number[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+	int index;
+
+	for (index = 0; index <= 9; index++)
+	{
+		putchar(numberz[index]);
+	}
+	putchar('\n');
+	return (0);
+}
