01d0f32ed2ae5546730750cd5502187f0e3d79ed
HAFSA519 <hafsa.fsac@gmail.com>
2023-06-20 15:45:08 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@
 void print_to_98(int n)
 {
 	int i, j;
-	
+
 	if (n <= 98)
 	{
 		for (i = n; i <= 98; i++)
@@ -21,14 +21,14 @@ void print_to_98(int n)
 					;
 		}
 	}
-       else if (n >= 98)
-       {
-	       for (j = n; j >= 98; j--)
-	       {
-		       if (j != 98)
-			       printf("%d, ", j);
-		       else if (j == 98)
-			       printf("%d\n", j);
-	       }
-       }
+	else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
+		}
+	}
 }
