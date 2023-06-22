fcc13bca0b393157260cf26230dfa9b523728d49
HAFSA519 <hafsa.fsac@gmail.com>
2023-06-22 12:04:54 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,10 +5,11 @@
  * @x: The number to be checked
  * Return: 1 for upper letter or 0 for any else
  */
+
 int _isupper(int x)
 
 {
-    if (x >= 65 && x <= 90)
+    if (x >= 'A' && x <= 'Z')
     {
 	    return (1);
     }
