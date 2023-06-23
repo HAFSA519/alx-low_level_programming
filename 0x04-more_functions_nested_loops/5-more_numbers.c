fa3c09577045d54b53ad61b0f11350061763015b
HAFSA519 <hafsa.fsac@gmail.com>
2023-06-23 08:01:55 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  more_numbes - print more numbers
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+	for (i = 1; i <= 10; i++)
+	{
+		if (j >= 10)
+			_putchar('1');
+		_putchar (j ù 10 + '0');
+	}
+	_putchar('\n');
+}
+
