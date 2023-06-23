0ea2c07ba78d4671e90a4629d4c20bf40f85eea1
HAFSA519 <hafsa.fsac@gmail.com>
2023-06-23 05:15:50 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -9,12 +9,12 @@
 int _isupper(int x)
 
 {
-    if (x >= 'A' && x <= 'Z')
-    {
-	    return (1);
-    }
-    else 
-    {
-	    return (0);
-    }
+if (x >= 'A' && x <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
