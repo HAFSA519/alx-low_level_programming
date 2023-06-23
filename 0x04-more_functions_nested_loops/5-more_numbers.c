4954ce73137ebe9770f8476a3c718b4922a9784b
HAFSA519 <hafsa.fsac@gmail.com>
2023-06-23 15:10:48 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  *  more_numbes - print more numbers
@@ -8,13 +9,17 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 1; i <= 10; i++)
+	for (i = 0; i <= 14; i++)
 	{
-		if (j >= 10)
-			_putchar('1');
-
+		if (j < 10)
+		{
+			_putchar(j + '0');
+		}
+		else
+		{
+			putchar ('1');
 		_putchar (j % 10 + '0');
+		}
 	}
 	_putchar('\n');
 }
-
