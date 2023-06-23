88daa67f8f63833e6db5589f7d588fafadbf180d
HAFSA519 <hafsa.fsac@gmail.com>
2023-06-23 15:52:00 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,16 +7,16 @@
 
 void print_line(int n)
 {
+	int i;
+
 	if (n <= 0)
 	{
 		putchar('\n');
 		return;
 	}
-	int i;
-
 	for (i = 0; i < n; i++)
 	{
-		_putchar('_');
+		putchar('_');
 	}
 	_putchar('\n');
 }
