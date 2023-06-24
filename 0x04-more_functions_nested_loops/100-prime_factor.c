3b957ebbf7d4da1a0a8283cb903376038722a980
HAFSA519 <hafsa.fsac@gmail.com>
2023-06-23 17:11:07 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,7 +10,7 @@ void print_triangle(int size)
 {
 	if (size <= 0)
 	{
-		_putchar('\n');
+	putchar('\n');
 	}
 	else
 	{
@@ -20,14 +20,14 @@ void print_triangle(int size)
 			 {
 				 for (j = i; j < size; j++)
 				 {
-					 _putchar(' ');
+				putchar(' ');
 				 }
 
 				 for (j = 1; j <= i; j++)
 				 {
-					 _putchar('#');
+				putchar('#');
 				 }
-				 _putchar('\n');
+				 putchar('\n');
 			 }
 		 }
 }
