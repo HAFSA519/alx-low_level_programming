2e7c386be851b8c62e2910323155d5ec3ce714bb
HAFSA519 <hafsa.fsac@gmail.com>
2023-06-23 17:24:28 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,28 +6,32 @@
  * @size: size of the triangle
  */
 
-void print_triangle(int size)
+int main(void)
 {
-	if (size <= 0)
-	{
-	putchar('\n');
-	}
-	else
-	{
-			 int i, j;
+long long int number = 612852475143;
+long long int largestPrimeFactor = 0;
 
-			 for (i = 1; i <= size; i++)
-			 {
-				 for (j = i; j < size; j++)
-				 {
-				putchar(' ');
-				 }
-
-				 for (j = 1; j <= i; j++)
-				 {
-				putchar('#');
-				 }
-				 putchar('\n');
-			 }
-		 }
+while (number % 2 == 0)
+{
+largestPrimeFactor = 2;
+number /= 2;
 }
+
+for (long long int i = 3; i * i <= number; i += 2)
+{
+while (number % i == 0)
+{
+largestPrimeFactor = i;
+number /= i;
+}
+}
+
+if (number > 2)
+{
+largestPrimeFactor = number;
+}
+printf("%lld\n", largestPrimeFactor);
+
+return (0);
+}
+
