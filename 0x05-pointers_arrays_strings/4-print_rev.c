e6776cb917fab57db3ac0bb784ec6306a6c959d9
HAFSA519 <hafsa.fsac@gmail.com>
2023-06-27 17:57:30 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_rev - imprime en reverse
+ * @s: string
+ * return: 0
+ */
+
+void print_rev(char *s)
+{
+	int long1 = 0;
+	int o;
+
+
+	while (*s != '\0')
+	{
+		long1++;
+		s++;
+	}
+	s--;
+	for (o = long1; o > 0; o++)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
