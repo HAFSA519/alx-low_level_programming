12b243a58181d206057e251d30f2ceae4a190903
HAFSA519 <hafsa.fsac@gmail.com>
2023-07-14 19:01:48 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,16 +1,15 @@
-#include <stdlib.h>		
+#include <stdlib.h>
 #include "main.h"
-		
 /**
  * *_realloc - reallocates a memory block using malloc and free
  * @ptr: pointer to the memory previsouly allocated by malloc
  * @old_size: size of the allocated memory for ptr
- * @new_size: new size of the new memory block		
+ * @new_size: new size of the new memory block
  *
- * Return: pointer to the newly allocated memory block		
+ * Return: pointer to the newly allocated memory block
  */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)	
-{	
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
 	char *ptr1;
 
 	char *old_ptr;
@@ -19,10 +18,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == old_size)
 		return (ptr);
-	if (new_size == 0 && ptr)	
+	if (new_size == 0 && ptr)
 	{
 		free(ptr);
-		return (NULL);	
+		return (NULL);
 	}
 	if (!ptr)
 		return (malloc(new_size));
@@ -30,16 +29,16 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (!ptr1)
 		return (NULL);
 	old_ptr = ptr;
-	if (new_size < old_size)	
+	if (new_size < old_size)
 	{
 		for (i = 0; i < new_size; i++)
-			ptr1[i] = old_ptr[i];	
+			ptr1[i] = old_ptr[i];
 	}
-	if (new_size > old_size)	
+	if (new_size > old_size)
 	{
 		for (i = 0; i < old_size; i++)
-			ptr1[i] = old_ptr[i];	
+			ptr1[i] = old_ptr[i];
 	}
 	free(ptr);
-	return (ptr1);	
+	return (ptr1);
 }
